08a361a95f6312b010dd215365a1c11eb75e56c3
yuxxeun <itsyuxxeun@gmail.com>
2025-05-17 14:15:51 +0700

    format

--- models/setup.go
+++ models/setup.go
@@ -12,28 +12,28 @@ import (
 
 var DB *gorm.DB
 
-	func ConnectDatabase() {
-		// Load .env file
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
-	
-		// Ambil dari environment
-		user := os.Getenv("DB_USER")
-		pass := os.Getenv("DB_PASS")
-		host := os.Getenv("DB_HOST")
-		port := os.Getenv("DB_PORT")
-		name := os.Getenv("DB_NAME")
-	
-		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, pass, host, port, name)
-		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-		if err != nil {
-			log.Fatal("Gagal konek ke database: ", err)
-		}
-	
-		// AutoMigrate model kamu
-		db.AutoMigrate(&Bagian{}) // Panggil package models
-		DB = db
-	
+func ConnectDatabase() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Ambil dari environment
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASS")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, pass, host, port, name)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Gagal konek ke database: ", err)
+	}
+
+	// AutoMigrate model kamu
+	db.AutoMigrate(&Bagian{}) // Panggil package models
+	DB = db
+
 }
