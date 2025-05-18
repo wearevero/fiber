877794ffabe169174f6567af8f6d5a2117fb3af1
yuxxeun <itsyuxxeun@gmail.com>
2025-05-18 12:58:28 +0700

    feat: jabatan resource

---
+++ models/jabatan.go
@@ -0,0 +1,10 @@
+package models
+
+type Jabatan struct {
+	IdJabatan int64  `gorm:"primaryKey;column:IdJabatan" json:"IdJabatan"`
+	Jabatan   string `gorm:"type:varchar(300);column:Jabatan" json:"Jabatan"`
+}
+
+func (Jabatan) TableName() string {
+	return "tjabatan"
+}
--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/wearevero/fiber/controllers/bagiancontroller"
+	"github.com/wearevero/fiber/controllers/jabatancontroller"
 )
 
 func SetupRoutes(app *fiber.App) {
@@ -11,11 +12,19 @@ func SetupRoutes(app *fiber.App) {
 
 	master_data := v1.Group("/master-data")
 	bagian := master_data.Group("/bagian")
+	jabatan := master_data.Group("jabatan")
 
-	// Define routes
+	// Define routes bagian
 	bagian.Get("/", bagiancontroller.Index)
 	bagian.Get("/:IdBagian", bagiancontroller.Show)
 	bagian.Post("/", bagiancontroller.Create)
 	bagian.Put("/:IdBagian", bagiancontroller.Update)
 	bagian.Delete("/:IdBagian", bagiancontroller.Delete)
+
+	// Define routes jabatan
+	jabatan.Get("/", jabatancontroller.Index)
+	jabatan.Get("/:IdJabatan", jabatancontroller.Show)
+	jabatan.Post("/", jabatancontroller.Create)
+	jabatan.Put("/:IdJabatan", jabatancontroller.Update)
+	jabatan.Delete("/:IdJabatan", jabatancontroller.Delete)
 }
